94fd7a2910b3619543447d80673d7db31b2696d3
Tavo <gariosp@unal.edu.co>
2017-08-22 17:55:04 -0300

    Adding comunication to NYTimes API

--- js/script.js
+++ js/script.js
@@ -16,19 +16,52 @@ function loadData() {
     var googleStreetViewImage = "http://maps.googleapis.com/maps/api/streetview?size=600x300&location=";
     var $city = $('#city').val();
     var $street =$('#street').val();
-
-    console.log("city: " + $city);
-    console.log("street: " + $street);
-
+    //console.log("city: " + $city);
+    //console.log("street: " + $street);
     var $address = $city + ', ' + $street;
-
     googleStreetViewImage += $address;
-
     $greeting.text("So you want to live at " + $address + "!");
-
-
     $body.append('<img class="bgimg" src="' + googleStreetViewImage + '">');
-    // YOUR CODE GOES HERE!
+
+    //Load NYTimes data
+
+
+    var NYTimesURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    NYTimesURL += '?' + $.param({
+      'api-key': "1315706dd301431ebc3f3de8fa11d2e6",
+      'q': "buenos aires"
+    });
+
+    $.getJSON( NYTimesURL, function( data ) 
+    {
+        var items = [];
+        console.log(data.response.docs);
+        var news = data.response.docs;
+        $.each( news, function( key, val )
+        {
+            items.push( "<li id='article'>");
+            items.push("<a href=" + val.web_url +">");
+            if (key != 0)
+            {
+                items.push(val.headline.print_headline);
+            }
+            else
+            {
+                items.push("Overview of " + $city);
+            }
+            items.push("</a>");
+            items.push("<p>" + val.snippet + "</p>");
+            items.push("</li>")
+        });
+        $('#nytimes-articles-default').detach();
+        $( "<ul/>", 
+        {
+            "id": "nytimes-articles",
+            "class": "article-list",
+            html: items.join( "" )
+        }).appendTo( ".nytimes-container" );
+    });
+    
 
     return false;
 };
