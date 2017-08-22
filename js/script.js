a56f10fdf65f117e9b9c9e9bb86e7e3c080d9bf7
Tavo <gariosp@unal.edu.co>
2017-08-22 15:08:48 -0300

    Adding Google street view background image

--- js/script.js
+++ js/script.js
@@ -13,6 +13,21 @@ function loadData() {
 
     // load streetview
 
+    var googleStreetViewImage = "http://maps.googleapis.com/maps/api/streetview?size=600x300&location=";
+    var $city = $('#city').val();
+    var $street =$('#street').val();
+
+    console.log("city: " + $city);
+    console.log("street: " + $street);
+
+    var $address = $city + ', ' + $street;
+
+    googleStreetViewImage += $address;
+
+    $greeting.text("So you want to live at " + $address + "!");
+
+
+    $body.append('<img class="bgimg" src="' + googleStreetViewImage + '">');
     // YOUR CODE GOES HERE!
 
     return false;
